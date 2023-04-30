cd84f85dbe9e59ed768e1f97551af5e499f9aa8e
nullskill <22912338+nullskill@users.noreply.github.com>
2023-04-30 19:38:27 +0300

    Add Longest Common Prefix solution

---
+++ easy/14. Longest Common Prefix/main.dart
@@ -0,0 +1,32 @@
+/// The algorithm uses `for` loop to iterate through each character of the strings,
+/// so the time complexity is `O(n)`. In each iteration of the loop, we compare the characters
+/// of the strings at the same position until there is a mismatch or one of the strings ends.
+/// We then update the prefix to the common characters and continue to the next string.
+/// If at any point the prefix becomes empty, we know that there is no common prefix and return an empty string.
+
+class Solution {
+  String longestCommonPrefix(List<String> strs) {
+    if (strs.isEmpty) return '';
+    String prefix = strs[0];
+    for (int i = 1; i < strs.length; i++) {
+      while (strs[i].indexOf(prefix) != 0) {
+        prefix = prefix.substring(0, prefix.length - 1);
+        if (prefix.isEmpty) return '';
+      }
+    }
+    return prefix;
+  }
+}
+
+void main(List<String> args) {
+  final x = ["flower", "flow", "flight"];
+
+  final result = Solution().longestCommonPrefix(x);
+
+  print(result);
+}
+
+/// NB: Despite the algorithm has nested `while` loop, 
+/// but the number of iterations is still proportional to the length of the input strings, 
+/// so the overall time complexity is still `O(n)`, where `n` is the total number of characters in all strings.
+
