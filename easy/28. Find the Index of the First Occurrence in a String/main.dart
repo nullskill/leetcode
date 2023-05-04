d17c42644b7b60c90291da245aebf8ecd7dbe29a
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-04 13:37:10 +0300

    Add Find the Index of the First Occurrence in a String solution

---
+++ easy/28. Find the Index of the First Occurrence in a String/main.dart
@@ -0,0 +1,40 @@
+/// This solution works by iterating over each character in haystack
+/// and comparing it with the first character in needle.
+/// If there is a match, it then compares the remaining characters
+/// in needle with the corresponding characters in haystack.
+/// If all characters match, it returns the index of the first occurrence
+/// of needle in haystack.
+/// Otherwise, it continues iterating over the remaining characters in haystack.
+
+class Solution {
+  int strStr(String haystack, String needle) {
+    if (needle.isEmpty) return 0;
+    if (haystack.isEmpty) return -1;
+
+    for (var i = 0; i < haystack.length - needle.length + 1; i++) {
+      if (haystack[i] == needle[0]) {
+        var j = 0;
+        while (j < needle.length && haystack[i + j] == needle[j]) {
+          j++;
+        }
+        if (j == needle.length) return i;
+      }
+    }
+
+    return -1;
+  }
+}
+
+void main(List<String> args) {
+  final haystack = "sadbutsad";
+  final needle = "sad";
+
+  final result = Solution().strStr(haystack, needle);
+
+  print(result);
+}
+
+/// NB: This solution is `O(n)` because it iterates over each character in haystack exactly once. 
+/// The worst-case scenario is when the first character in needle matches the last character in haystack, 
+/// which means that the inner loop will run needle.length times for each iteration of the outer loop. 
+/// Therefore, the time complexity is `O(n)`.
