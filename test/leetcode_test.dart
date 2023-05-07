883d35ed9a048a116328484eeb5602b37208c0e7
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-07 20:58:14 +0300

    Rename group of tests

--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -1,10 +1,9 @@
 import 'package:test/test.dart';
 
-///
 import 'easy/1.two_sum.dart' as two_sum;
 
 void main() {
-  group('problems', () {
+  group('solutions', () {
     two_sum.main();
 
     // TODO: add more tests
