8e6ef1b563c1ce34171e3c6f624a8c716c181b96
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 19:21:08 +0300

    Add method `of()` and equality operator

--- lib/src/structure/list_node.dart
+++ lib/src/structure/list_node.dart
@@ -4,8 +4,29 @@ class ListNode {
 
   ListNode([this.val = 0, this.next]);
 
+  static ListNode? of(Iterable<int> collection) {
+    final iterator = collection.iterator;
+    if (!iterator.moveNext()) return null;
+    final head = ListNode(iterator.current);
+    var node = head;
+    while (iterator.moveNext()) {
+      node = node.next = ListNode(iterator.current);
+    }
+    return head;
+  }
+
   @override
   String toString() {
     return '$val${next == null ? '' : ', $next'}';
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ListNode && other.val == val;
+  }
+
+  @override
+  int get hashCode => val.hashCode;
 }
