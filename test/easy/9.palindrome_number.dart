75cf084f94384d1f0f2cb6e750637c1bf3b07088
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 14:50:18 +0300

    Tests for 9. Palindrome Number

---
+++ test/easy/9.palindrome_number.dart
@@ -0,0 +1,36 @@
+import 'package:leetcode/src/easy/9.palindrome_number/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('two_sum', () {
+    final f = Solution().isPalindrome;
+
+    test('f(0)', () {
+      expect(f(0), true);
+    });
+
+    test('f(111)', () {
+      expect(f(111), true);
+    });
+
+    test('f(121)', () {
+      expect(f(121), true);
+    });
+
+    test('f(-121)', () {
+      expect(f(-121), false);
+    });
+
+    test('f(10)', () {
+      expect(f(10), false);
+    });
+
+    test('f(123)', () {
+      expect(f(123), false);
+    });
+
+    test('f(-231)', () {
+      expect(f(-231), false);
+    });
+  }); // group 'two_sum'
+}
