0e98170e52f6c299e7125dc470a503e3b0aca6b2
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 17:22:17 +0300

    Tests for 14. Longest Common Prefix

---
+++ test/easy/14.longest_common_prefix.dart
@@ -0,0 +1,28 @@
+import 'package:leetcode/src/easy/14.longest_common_prefix/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('longest_common_prefix', () {
+    final f = Solution().longestCommonPrefix;
+
+    test('f([])', () {
+      expect(f([]), '');
+    });
+
+    test('f(["foo", "bar", "baz", "qux"])', () {
+      expect(f(['foo', 'bar', 'baz', 'qux']), '');
+    });
+
+    test('f(["flower", "flow", "flight"])', () {
+      expect(f(['flower', 'flow', 'flight']), 'fl');
+    });
+
+    test('f(["dog","racecar","car"])', () {
+      expect(f(['dog', 'racecar', 'car']), '');
+    });
+
+    test('f(["submission", "subtraction", "substitution", "synchronization"])', () {
+      expect(f(['submission', 'subtraction', 'substitution', 'synchronization']), 's');
+    });
+  }); // group 'two_sum'
+}
--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -2,6 +2,7 @@ import 'package:test/test.dart';
 
 import 'easy/1.two_sum.dart' as two_sum;
 import 'easy/13.roman_to_integer.dart' as roman_to_integer;
+import 'easy/14.longest_common_prefix.dart' as longest_common_prefix;
 import 'easy/9.palindrome_number.dart' as palindrome_number;
 
 void main() {
@@ -9,5 +10,6 @@ void main() {
     two_sum.main();
     palindrome_number.main();
     roman_to_integer.main();
+    longest_common_prefix.main();
   });
 }
