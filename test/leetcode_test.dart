8bd19e18a2ea38a544d5aff8a20e93619f5012fb
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 19:45:06 +0300

    Rename test files

--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -1,11 +1,11 @@
 import 'package:test/test.dart';
 
-import 'easy/1.two_sum.dart' as two_sum;
+import 'easy/1.two_sum.test.dart' as two_sum;
 import 'easy/13.roman_to_integer.dart' as roman_to_integer;
 import 'easy/14.longest_common_prefix.dart' as longest_common_prefix;
 import 'easy/20.valid_parentheses.dart' as valid_parentheses;
 import 'easy/21.merge_two_sorted_lists.dart' as merge_two_sorted_lists;
-import 'easy/9.palindrome_number.dart' as palindrome_number;
+import 'easy/9.palindrome_number.test.dart' as palindrome_number;
 
 void main() {
   group('solution', () {
