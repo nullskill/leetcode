44e9573979ebfa968418ea1b412b8f5c60cb78df
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 15:01:20 +0300

    Update tests

--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -1,11 +1,11 @@
 import 'package:test/test.dart';
 
 import 'easy/1.two_sum.dart' as two_sum;
+import 'easy/9.palindrome_number.dart' as palindrome_number;
 
 void main() {
   group('solutions', () {
     two_sum.main();
-
-    // TODO: add more tests
+    palindrome_number.main();
   });
 }
