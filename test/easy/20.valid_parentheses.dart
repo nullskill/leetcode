4c1eb17fc1d32058e6809c278f2f7d9b3498c794
nullskill <22912338+nullskill@users.noreply.github.com>
2023-05-09 17:35:59 +0300

    Tests for 20. Valid Parentheses

---
+++ test/easy/20.valid_parentheses.dart
@@ -0,0 +1,40 @@
+import 'package:leetcode/src/easy/20.valid_parentheses/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('valid_parentheses', () {
+    final f = Solution().isValid;
+
+    test('f("}")', () {
+      expect(f('}'), false);
+    });
+
+    test('f("()")', () {
+      expect(f('()'), true);
+    });
+
+    test('f("()[]{}")', () {
+      expect(f('()[]{}'), true);
+    });
+
+    test('f("(]")', () {
+      expect(f('(]'), false);
+    });
+
+    test('f("()([]")', () {
+      expect(f('()([]'), false);
+    });
+
+    test('f("()[])")', () {
+      expect(f('()[])'), false);
+    });
+
+    test('f("(()))")', () {
+      expect(f('(()))'), false);
+    });
+
+    test('f("([{}(){}])")', () {
+      expect(f('([{}(){}])'), true);
+    });
+  }); // group 'two_sum'
+}
--- test/leetcode_test.dart
+++ test/leetcode_test.dart
@@ -3,6 +3,7 @@ import 'package:test/test.dart';
 import 'easy/1.two_sum.dart' as two_sum;
 import 'easy/13.roman_to_integer.dart' as roman_to_integer;
 import 'easy/14.longest_common_prefix.dart' as longest_common_prefix;
+import 'easy/20.valid_parentheses.dart' as valid_parentheses;
 import 'easy/9.palindrome_number.dart' as palindrome_number;
 
 void main() {
@@ -11,5 +12,6 @@ void main() {
     palindrome_number.main();
     roman_to_integer.main();
     longest_common_prefix.main();
+    valid_parentheses.main();
   });
 }
