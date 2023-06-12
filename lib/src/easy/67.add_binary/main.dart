6cce6735696bbd2be44184d3dbad5e59e7f96c8f
nullskill <22912338+nullskill@users.noreply.github.com>
2023-06-12 17:03:46 +0300

    Refactor code

--- lib/src/easy/67.add_binary/main.dart
+++ lib/src/easy/67.add_binary/main.dart
@@ -7,21 +7,24 @@
 
 class Solution {
   String addBinary(String a, String b) {
-    int i = a.length - 1, j = b.length - 1, carry = 0;
-    StringBuffer sb = StringBuffer();
+    int codeZero = '0'.codeUnitAt(0);
+    int i = a.length - 1;
+    int j = b.length - 1;
+    int carry = 0;
+    final sb = StringBuffer();
     while (i >= 0 || j >= 0) {
       int sum = carry;
-      if (i >= 0) sum += a.codeUnitAt(i--) - '0'.codeUnitAt(0);
-      if (j >= 0) sum += b.codeUnitAt(j--) - '0'.codeUnitAt(0);
-      sb.writeCharCode(sum % 2 + '0'.codeUnitAt(0));
+      if (i >= 0) sum += a.codeUnitAt(i--) - codeZero;
+      if (j >= 0) sum += b.codeUnitAt(j--) - codeZero;
+      sb.writeCharCode(sum % 2 + codeZero);
       carry = sum ~/ 2;
     }
-    if (carry != 0) sb.writeCharCode(carry + '0'.codeUnitAt(0));
+    if (carry != 0) sb.writeCharCode(carry + codeZero);
     return sb.toString().split('').reversed.join('');
   }
 }
 
-void main(List<String> args) {
+void main() {
   String a = "11", b = "1";
   print(Solution().addBinary(a, b)); // "100"
 
