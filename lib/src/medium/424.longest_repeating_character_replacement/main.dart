b52cd114af77bfd19ec18e557a1f2763451f714d
nullskill <22912338+nullskill@users.noreply.github.com>
2023-06-21 12:08:17 +0300

    Add algorithm explanation

--- lib/src/medium/424.longest_repeating_character_replacement/main.dart
+++ lib/src/medium/424.longest_repeating_character_replacement/main.dart
@@ -7,6 +7,16 @@ import 'dart:math';
 /// The space complexity of the function is `O(1)`,
 /// since it uses a fixed-size list of length 26
 /// to store the count of each character in the string.
+///
+/// The algorithm uses a sliding window approach to find the length
+/// of the longest substring containing the same letter you can get
+/// after performing at most `k` character replacements. It maintains
+/// a count of the frequency of each letter in the current window
+/// and updates the maximum count seen so far. If the number of
+/// characters that need to be replaced to make all characters in
+/// the window the same exceeds `k`, it shrinks the window from the
+/// left side. The length of the window is updated at each step and
+/// the maximum length seen so far is returned at the end.
 
 class Solution {
   int characterReplacement(String s, int k) {
